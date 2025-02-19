b7702f6118f6e5baeb3a04e8ab2e90da97a2d769
Paul Trew <paultrew@outlook.com>
2025-02-19 10:42:09 +0000

    Added Big to Hello World

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,7 +5,7 @@ using namespace std;
 Here they are */
 int main()
 {
-    cout << "Hello World \n";
+    cout << "Hello Big World \n";
     cout << "Have a good day!" << "\n";
     cout << 3 << "\n";
     cout << 2 * 5 << "\n";
